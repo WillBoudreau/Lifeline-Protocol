f05a1adf52423810931b04332a71857c63b9e74f
WillBoudreau <williamboudreau804@gmail.com>
2026-03-14 21:51:57 -0300

    Loading screen and main menu added

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -1,15 +1,64 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class LevelManager : MonoBehaviour
 {
-    /// <summary>
-    /// Loads the specified scene
-    /// </summary>
-    /// <param name="sceneName">The name of the scene to load</param>
-    public void LoadScene(string sceneName)
+    [Header("Level Settings")]
+    [SerializeField] private float levelLoadDelay = 2f;
+    [SerializeField] private string nextLevelName;
+    public List<AsyncOperation> scenesToLoad = new List<AsyncOperation>();
+    public int activeLevelNumber;
+    public string sceneName;
+    [Header("Level References")]
+    [SerializeField] private UIManager uiManager;
+
+    private void Start()
     {
-        SceneManager.LoadScene(sceneName);
+        if (uiManager == null)
+        {
+            uiManager = FindObjectOfType<UIManager>();
+        }
+    }
+
+    /// <summary>
+    /// Loads the level based on the sceneName variable
+    /// </summary>
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        uiManager.UILoadingScreen(uiManager.loadingScreen);
+        StartCoroutine(WaitForScreenLoad(sceneName));
+    }
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == sceneName)
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+    }
+    /// <summary>
+    /// Waits for the screen to load before starting the laoding process
+    /// </summary>
+    private IEnumerator WaitForScreenLoad(string sceneName)
+    {
+        
+        yield return new WaitForSeconds(uiManager.loadSpeed);
+
+        yield return new WaitForSeconds(levelLoadDelay);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+        asyncLoad.completed += OperationCompleted;
+        scenesToLoad.Add(asyncLoad);
+    }
+    /// <summary>
+    /// Called when the scene has finished loading
+    /// </summary>
+    private void OperationCompleted(AsyncOperation obj)
+    {
+        scenesToLoad.Remove(obj);
+        Debug.Log("Scene Loaded");
     }
 }
--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,5 +1,11 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+using System.Diagnostics.Contracts;
 
 public class UIManager : MonoBehaviour
 {
@@ -10,7 +16,20 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject gameOverMenu;
     [SerializeField] private GameObject inventoryUI;
     [SerializeField] private GameObject characterStatsUI;
-  
+    public GameObject loadingScreen;
+
+    [Header("Loading Screen Settings")]
+    [SerializeField] private float loadingScreenFadeDuration = 1f;
+    public CanvasGroup loadingScreenCanvasGroup;
+    public Image loadingBar;
+    public float loadSpeed = 2f;
+
+    private void Start()
+    {
+        DisableAllUI();
+        mainMenu.SetActive(true);
+    }
+
     /// <summary>
     /// Sets all the UIs to false
     /// </summary>
@@ -22,6 +41,7 @@ public class UIManager : MonoBehaviour
         inventoryUI.SetActive(false);
         characterStatsUI.SetActive(false);
         gameplayUI.SetActive(false);
+        loadingScreen.SetActive(false);
     }
     /// <summary>
     /// Sets the specified UI to true
@@ -50,9 +70,82 @@ public class UIManager : MonoBehaviour
             case "GameplayUI":
                 gameplayUI.SetActive(true);
                 break;
+            case "LoadingScreen":
+                loadingScreen.SetActive(true);
+                break;
             default:
                 Debug.LogWarning("UIManager: Unknown UI name: " + uiName);
                 break;
         }
+    }
+
+    /// <summary>
+    /// Starts the loading screen process
+    /// </summary>
+    public void UILoadingScreen(GameObject targetUI)
+    {
+        StartCoroutine(LoadingScreenFadeIn());
+        StartCoroutine(WaitForLoadingScreen(loadingScreenFadeDuration, targetUI));
+    }
+    /// <summary>
+    /// Handles the loading screen fade in
+    /// </summary>
+    private IEnumerator LoadingScreenFadeIn()
+    {
+        DisableAllUI();
+        float timer = 0f;
+
+        loadingScreen.SetActive(true);
+        while (timer < loadingScreenFadeDuration)
+        {
+            timer += Time.deltaTime;
+            loadingScreenCanvasGroup.alpha = Mathf.Lerp(0f, 1f, timer / loadingScreenFadeDuration);
+            yield return null;
+        }
+        StartCoroutine(FillLoadingBar());
+    }
+    /// <summary>
+    /// Handles the loading screen fade out
+    /// </summary>
+    private IEnumerator LoadingScreenFadeOut()
+    {
+        float timer = 0f;
+
+        while (timer < loadingScreenFadeDuration)
+        {
+            timer += Time.deltaTime;
+            loadingScreenCanvasGroup.alpha = Mathf.Lerp(1f, 0f, timer / loadingScreenFadeDuration);
+            yield return null;
+        }
+        loadingScreenCanvasGroup.alpha = 0f;
+        loadingScreen.SetActive(false);
+        loadingBar.fillAmount = 0f;
+    }
+    /// <summary>
+    /// Waits for the loading screen to finish fading in before enabling the target UI
+    /// <param name="waitTime">The time to wait before enabling the target UI</param>
+    /// <param name="targetUI">The UI to enable after the loading screen</param>
+    /// </summary>
+    private IEnumerator WaitForLoadingScreen(float waitTime, GameObject targetUI)
+    {
+        yield return new WaitForSeconds(waitTime);
+        DisableAllUI();
+        targetUI.SetActive(true);
+    }
+    ///<summary>
+    /// Updates the loading bar fill amount
+    /// </summary>
+    public IEnumerator FillLoadingBar()
+    {
+        float timer = 0f;
+        loadingBar.fillAmount = 0f;
+        while (timer < 1f)
+        {
+            timer += Time.deltaTime/2;
+            loadingBar.fillAmount += Time.deltaTime/2;
+            yield return null;
+        }
+        yield return new WaitForEndOfFrame();
+        StartCoroutine(LoadingScreenFadeOut());
     }
 }
